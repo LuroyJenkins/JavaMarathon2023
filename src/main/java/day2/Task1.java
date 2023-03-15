1121b1fa4a99c0095a8843d8be4906f90278962f
Nikita Starkov <starkfinity@gmail.com>
2023-03-15 22:33:04 +0500

    added day 2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,29 @@
 package day2;
 
+import java.util.Scanner;
+
+/**
+ * @author Neil Alishev
+ * @student Nikita Starkov
+ *
+ * Реализовать программу, которая принимает на вход через консоль с помощью класса Scanner, число, соответствующее
+ * количеству этажей в здании. Используя условный оператор if, необходимо вывести в консоль сообщение о типе такого дома.
+ * Условия: если этажей 1-4 - “Малоэтажный дом”, 5-8 - “Среднеэтажный дом”, 9 и более - “Многоэтажный дом”.
+ * Также, необходимо учесть что может быть введено отрицательное значение, в таком случае сообщить “Ошибка ввода”.
+ */
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int floorCount = scanner.nextInt();
+
+        if (0 < floorCount && floorCount < 5)
+            System.out.println("Малоэтажный дом");
+        else if (floorCount > 4 && floorCount < 9)
+            System.out.println("Среднеэтажный дом");
+        else if (floorCount > 8)
+            System.out.println("Многоэтажный дом");
+        else
+            System.out.println("Ошибка ввода");
 
     }
 }
--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,28 @@
 package day2;
 
+import java.util.Scanner;
+
+/**
+ * @author Neil Alishev
+ * @student Nikita Starkov
+ *
+ * Реализовать программу, которая принимает на вход через консоль с помощью класса Scanner, число x. Для этого числа,
+ * по формуле выше, необходимо вычислить значение y и вывести его в консоль, используя метод System.out.println().
+ */
+
 public class Task4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        double y;
 
+        if (x >= 5)
+            y = (Math.pow(x, 2) - 10) / (x + 7);
+        else if (x > -3 && x < 5)
+            y = (x + 3) * (Math.pow(x, 2) - 2);
+        else
+            y = 420;
+
+        System.out.println(y);
     }
 }
