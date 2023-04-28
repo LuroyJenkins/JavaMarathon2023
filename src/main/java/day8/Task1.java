2d8911d4fdf36e498faeb2d5d9df9766997c852b
Nikita Starkov <starkfinity@gmail.com>
2023-04-28 15:41:53 +0500

    Added day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,20 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String string = "";
+        long startTime = System.currentTimeMillis();
+        for(int i = 0; i <= 20000; i++){
+            string = string + i + " ";
+        }
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime-startTime));
 
+        StringBuilder sb = new StringBuilder();
+        startTime = System.currentTimeMillis();
+        for(int i = 0; i <= 20000; i++){
+            sb.append(i + " ");
+        }
+        stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime-startTime));
     }
 }
