7c976a20de1a3ae63f92aedbfef91d7e0d5eebd8
Nikita Starkov <starkfinity@gmail.com>
2023-05-11 14:12:08 +0500

    Added for day17 tasks.

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -2,6 +2,18 @@ package day17;
 
 public class Task1 {
     public static void main(String[] args) {
+        ChessPiece[] chessPieces =  new ChessPiece[8];
 
+        for (int i = 0; i < 4; i++){
+            chessPieces[i] = ChessPiece.PAWN_WHITE;
+        }
+
+        for(int i = 4; i < 8; i++){
+            chessPieces[i] = ChessPiece.ROOK_BLACK;
+        }
+
+        for (ChessPiece chessPiece: chessPieces) {
+            System.out.print(chessPiece.getName() + " ");
+        }
     }
 }
